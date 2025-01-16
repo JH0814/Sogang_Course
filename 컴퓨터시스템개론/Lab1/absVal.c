b9d227397e0c54d58a4c0263843a8d0fba961d55
JH0814 <40225003+JH0814@users.noreply.github.com>
2025-01-16 11:58:25 +0900

    Add files via upload

---
+++ 컴퓨터시스템개론/Lab1/absVal.c
@@ -0,0 +1,8 @@
+/*
+ * absVal(x): return the absolute value of x.
+ * - Assume that INT_MIN (-2^31) is not provided as an input.
+ * - Ex) absVal(2) = 2, absVal(-1) = 1
+ */
+int absVal(int x) {
+  return (x^(x>>31))+((x>>31)&1); // TODO
+}
